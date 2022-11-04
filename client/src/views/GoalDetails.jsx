d987c88ee4899a2b5ddd8b5f9790988fd9ba884b
Jessica Greene <jessica.greene89@gmail.com>
2022-11-03 22:57:46 -0400

    updated goal detail page

--- client/src/views/GoalDetails.jsx
+++ client/src/views/GoalDetails.jsx
@@ -1,57 +1,79 @@
-import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import {useParams} from 'react-router-dom'
+import axios from 'axios'
 
 const GoalDetails = () => {
+  const [goal, setGoal] = useState({})
+  const{id} = useParams()
 
-  const [goal, setGoal] = useState({
-    'earnedStars':2, 
-    'totalStars':20
-  })
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/goals/' + id)
+      .then( res => {
+        setGoal(res.data)
+      })
+      .catch( err => console.log(err))
+  }, [id, goal])
+
   const starArray = []
-  for(let i=0;i<goal.earnedStars;i++){
-    starArray.push('earnedStar')
+  for(let i=0;i<goal.awardedStars;i++){
+    starArray.push('awardedStar')
   }
-  for(let i=0; i<goal.totalStars-goal.earnedStars;i++){
+  for(let i=0; i<goal.totalStars-goal.awardedStars;i++){
     starArray.push('')
   }
-  console.log(starArray)
 
   const awardStar = e => {
     e.preventDefault()
     console.log('addStar was clicked')
-    if(goal.earnedStars < goal.totalStars){
+    if(goal.awardedStars < goal.totalStars){
       const updatedGoal = {...goal}
-      updatedGoal['earnedStars'] = goal.earnedStars+1
-      setGoal(updatedGoal)
+      updatedGoal['awardedStars'] = goal.awardedStars+1
+      updateGoal(updatedGoal)
     }
+  }
+
+  const updateGoal = goalParam => {
+    axios.put('http://localhost:8000/api/goals/' + id, goalParam)
+      .then( res => {
+        console.log('Update was succesful', res.data)
+      })
+      .catch( err => {
+        console.log(err)
+      })
   }
 
   return (
     <div className="goal-details d-flex justify-content-center">
       <div className="d-flex flex-column align-items-center">
-          <h1 className="text-white">Clean Up Toys</h1>
-          <h2 className="text-white">“I will pick up my toys before bed.”</h2>
+          <h1 className="text-white">{goal.summary}</h1>
+          <p className="text-white"><em>"{goal.pledge}"</em></p>
           <div className='star-display mt-3 d-flex justify-content-center flex-wrap'>
             { starArray
-                .filter((star)=>(star === 'earnedStar'))
-                .map(star => (
-                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="goldenrod" className="bi bi-star-fill" viewBox="0 0 16 16">
+                .filter((star)=>(star === 'awardedStar'))
+                .map((star,i) => (
+                    <svg key={i} xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="goldenrod" className="bi bi-star-fill" viewBox="0 0 16 16">
                         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                     </svg>
             ))}
             { starArray
-                .filter((star)=>(star !== 'earnedStar'))
-                .map(star => (
-                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-star-fill" viewBox="0 0 16 16">
+                .filter((star)=>(star !== 'awardedStar'))
+                .map((star,i) => (
+                    <svg key={i} xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-star-fill" viewBox="0 0 16 16">
                         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                     </svg>
             ))}
           </div>
             <button onClick={awardStar} className='btn btn-success mt-3'> Award Star </button>
-          <div>
-            <img></img>
-            <h2 className="text-white">Reward</h2>
-            <p className="text-white">Little Orange Helicopter</p>
+          <div className='mt-5'>
+            {/* { (goal.reward || goal.rewardURL) &&
+              <h2 className="text-white">Reward</h2>
+            } */}
+            {goal.rewardURL &&
+              <img src={goal.rewardURL} alt='reward image' className='w-50'></img>
+            }
+            {goal.reward &&
+              <h4 className="text-white">{goal.reward}</h4>
+            }
         </div>
       </div>
     </div>
