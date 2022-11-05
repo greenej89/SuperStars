2693fec4b626ce2f567778e5e258ed79d13124fb
Jessica Greene <jessica.greene89@gmail.com>
2022-11-05 13:03:32 -0400

    display button to show access code

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,11 +6,12 @@ const Footer = ({accessCode, setAccessCode, validAccessCode}) => {
 
   const navigate = useNavigate()
   const [inputAccessCode, setInputAccessCode] = useState('')
+  const [displayError, setDisplayError] = useState(false)
 
   const requestAccessCode = e => {
-    alert(`Enter the access key:  ${validAccessCode}`)
+    alert(`The access code is:  ${validAccessCode}`)
     // setAccessCodeRequested(true)
-    }
+  }
 
   const accessControlCenter = e => {
     e.preventDefault()
@@ -23,24 +24,22 @@ const Footer = ({accessCode, setAccessCode, validAccessCode}) => {
   }
 
   return (
-    <div className="bg-dark text-white w-100 d-flex align-items-center justify-content-end">
+    <div className="bg-dark text-white w-100 d-flex align-items-center pt-1 justify-content-end">
         <form onSubmit={accessControlCenter} className="d-flex align-items-center justify-content-between me-2">
-                <label htmlFor="" className="col-form-label">
-                  <button className="btn rounded-circle">
-                    <svg onClick={requestAccessCode} 
-                        xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="red" className="bi bi-exclamation-circle" viewBox="0 0 16 16">
-                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                        <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"/>
-                    </svg>
-                  </button>Control Center Access
-                </label>
-                <div className="col-sm-5">
-                    <input type="text" className="form-control" id="inputEmail3" 
-                        onChange={e => setInputAccessCode(e.target.value)}
-                        value={inputAccessCode}
-                    />
-                </div>
-            <button type="submit" className="btn btn-info">Go</button>
+          <label htmlFor=""> Control Center Access Code</label>
+          <div className="col-sm-3">
+              <input type="text" className="form-control" id="inputEmail3" onChange={e => {setInputAccessCode(e.target.value)}} value={inputAccessCode}/>
+          </div>
+          <button type="submit" className="btn btn-info">Go</button>
+          {(inputAccessCode && inputAccessCode !== validAccessCode) &&
+              <button className="btn rounded-circle" onClick={requestAccessCode}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" className="bi bi-exclamation-circle" viewBox="0 0 16 16">
+                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                      <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"/>
+                </svg>
+                <p className='text-danger'>Show Access Code</p> 
+              </button>
+          }
         </form>
     </div>
   )
