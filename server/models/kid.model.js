ac79ce042af3f12c7a643f1b742d07e506dbbd81
Jessica Greene <jessica.greene89@gmail.com>
2022-11-25 12:13:39 -0500

    added cascade on delete for kid goals

--- server/models/kid.model.js
+++ server/models/kid.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const {Goal} = require('./goal.model')
     
 const KidSchema = new mongoose.Schema(
     {
@@ -23,6 +24,19 @@ const KidSchema = new mongoose.Schema(
     //     return value.length <= 5
     // }
 
+//Before the deleteOne function is run on the Kid
+KidSchema.pre('deleteOne', async function(next) {
+    //Retrieve the kid document's information for reference
+    const kidDocument = await this.model.findOne(this.getFilter()) 
+    //Try to delete all Goals that have an id listed in the Kid's goals array
+    try {
+        await Goal.deleteMany({_id: { $in: kidDocument.goals } })
+        next() //Then continue running the deleteOne function on Kid
+    } catch(err) {
+        console.log("Uh oh, something broke when deleting an associated goal", err)
+        next(err)
+    }
+})
 const Kid = mongoose.model('Kid', KidSchema)
     
 module.exports = {Kid}
